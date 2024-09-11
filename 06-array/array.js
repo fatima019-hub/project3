7d8d6d224fdb43ee37f2b4166d75b8fa83612456
fatima <febadi019@gmail.com>
2024-09-11 13:56:17 +0330

    revers

--- 06-array/array.js
+++ 06-array/array.js
@@ -1,12 +1,12 @@
 // write your func here
 
 function Equal() {
-  let a = [12, 1, 8, 20, 5];
+  let a = [12, "1", 8, 20, 5];
   let b = [12, 1, 8, 20, 5];
 
-  if (a.length != b.length) return "False";
+  if (a.length !== b.length) return "False";
   else {
-    for (let i = 0; i < a.length; i++) if (a[i] != b[i]) return "False";
+    for (let i = 0; i < a.length; i++) if (a[i] !== b[i]) return "False";
     return "True";
   }
 }
